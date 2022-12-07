754feaa6b888f32a89284695d78ca04186bacefa
“Kalpesh <kalpeshpawar7875@gmail.com>
2022-12-07 22:48:52 +0530

    Linear Search Problems

--- src/DSA/Linear.java
+++ src/DSA/Linear.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class Linear {
     public static void main(String[] args) {
@@ -21,11 +22,29 @@ public class Linear {
         System.out.println(ans5);
 
         // Search in range
-        int[] arr = { 3, 2, 12, 4, 56, 7, 3 };
+        int[] arr = { 3, 2, 12, 4, 56, 7, -3 };
         int targetr = 4;
         int ans6 = searchInRange(arr, targetr, 1, 5);
         System.out.println(ans6);
 
+        // Search minimum number
+        int[] arr1 = { 3, 21, 4, 31, 2, 6, 7 };
+        System.out.println(min(arr1));
+        // Search maximum number
+        int[] arr2 = { 3, 21, 4, 31, 2, 6, 7 };
+        System.out.println(max(arr2));
+
+        // search in 2D array
+        int[][] arr3 = { { 13, 4, 2 },
+                { 4, 1, 7, 3 },
+                { 32, 4, 12, 1, 31 } };
+
+        int target2d = 7;
+        int[] res = search2D(arr3, target2d);
+        System.out.println(Arrays.toString(res));
+
+        // search max in 2d
+        System.out.println(max2d(arr3));
     }
 
     // return index
@@ -115,5 +134,59 @@ public class Linear {
             }
         }
         return -1;
+    }
+
+    // Find minimum number
+
+    static int min(int[] arr) {
+        int i;
+        int ans = arr[0];
+        for (i = 1; i < arr.length; i++) {
+            if (arr[i] < ans) {
+                ans = arr[i];
+            }
+        }
+        return ans;
+
+    }
+
+    // Search minimum number
+    static int max(int[] arr) {
+        int i;
+        int ans = arr[0];
+        for (i = 1; i < arr.length; i++) {
+            if (arr[i] > ans) {
+                ans = arr[i];
+            }
+        }
+        return ans;
+
+    }
+
+    // Search in 2D array
+
+    static int[] search2D(int[][] arr, int target) {
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                if (arr[row][col] == target) {
+                    return new int[] { row, col };
+                }
+            }
+        }
+        return new int[] { -1, -1 };
+    }
+
+    // search max in 2D
+    static int max2d(int[][] arr) {
+        int maxm = Integer.MIN_VALUE;
+        for (int[] ints : arr) {
+            for (int element : ints) {
+                if (element > maxm) {
+                    maxm = element;
+                }
+            }
+        }
+        return maxm;
+
     }
 }
---
+++ src/Java Problem Set/EvenDigits.java
@@ -0,0 +1,53 @@
+public class EvenDigits {
+    public static void main(String[] args) {
+        int[] num = { 23, 2, 1, 223, 13232, 2222 };
+        int res = findnum(num);
+        System.out.println(res);
+        System.out.println(digits(3321));
+    }
+
+    static int findnum(int[] nums) {
+        int count = 0;
+        for (int num : nums) {
+            if (even(num)) {
+                count++;
+            }
+        }
+        return count;
+
+    }
+
+    // Function to check number even or odd
+    static boolean even(int num) {
+        int numOfdigits = digits2(num);
+        return (numOfdigits % 2) == 0;
+    }
+
+    // Optimize Function to count number of digits
+    static int digits(int num) {
+        if (num < 0) {
+            num = num * -1;
+        }
+        if (num == 0) {
+            return 1;
+        }
+        return (int) (Math.log10(num)) + 1;
+    }
+
+    // Another Function to count number of digits
+    static int digits2(int num) {
+        int count = 0;
+        if (num < 0) {
+            num = num * -1;
+        }
+        if (num == 0) {
+            return 1;
+        }
+
+        while (num > 0) {
+            count++;
+            num = num / 10;
+        }
+        return count;
+    }
+}
